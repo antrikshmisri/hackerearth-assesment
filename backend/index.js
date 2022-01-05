a37176957193933c35c74a84980a8b68a5e5855c
antrikshmisri <antrikshmisri@gmail.com>
2022-01-05 20:05:44 +0530

    fix: index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,6 @@ const methodOverride = require("method-override");
 
 app.use(bodyParser.json());
 app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 mongoose
   .connect(process.env.DATABASE_URI, { useNewUrlParser: true })
@@ -70,10 +69,10 @@ app.delete("/api/delete/:id", async (req, res) => {
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
   });
 }
 
