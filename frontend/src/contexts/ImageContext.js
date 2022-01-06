5f857977dd7df3a32d28f1336c578d2cf6afd9d6
antrikshmisri <antrikshmisri@gmail.com>
2022-01-06 13:01:35 +0530

    feat: loading state for allImages global state

--- frontend/src/contexts/ImageContext.js
+++ frontend/src/contexts/ImageContext.js
@@ -5,19 +5,22 @@ const ImageContext = React.createContext();
 
 export const ImageProvider = ({ children }) => {
   const [allImages, setAllImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     refreshAllImages();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   const refreshAllImages = () => {
+    setIsLoading(true);
     getAllImages().then((res) => {
       setAllImages([...res.data]);
+      setIsLoading(false);
     });
   };
 
   return (
-    <ImageContext.Provider value={{ allImages, refreshAllImages }}>
+    <ImageContext.Provider value={{ allImages, refreshAllImages, isLoading }}>
       {children}
     </ImageContext.Provider>
   );
--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -6,15 +6,19 @@ import { useNavigate } from "react-router-dom";
 import FloatingButton from "../components/FloatingButton";
 import { useImageContext } from "../contexts/ImageContext";
 import PaginatedComponent from "../components/PaginatedComponent";
+import HashLoader from "react-spinners/HashLoader";
 
 const Home = () => {
-  const { allImages } = useImageContext();
+  const { allImages, isLoading } = useImageContext();
   const navigate = useNavigate();
 
   return (
     <>
       <Container fluid>
         <Row className="card-container">
+          <div className={isLoading ? "loading-overlay" : ""}>
+            <HashLoader loading={isLoading} color="#ED4264" />
+          </div>
           <PaginatedComponent
             itemsPerPage={9}
             totalItems={allImages.length}
